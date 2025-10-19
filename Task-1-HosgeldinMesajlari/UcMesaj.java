5ddc60eaa94096c4412925c3668bc2ed37b77be2
Nisanur <nisaeltekin@gmail.com>
2025-10-19 17:11:25 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,9 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+        System.out.println("Proglamlama Eğlencelidir");
+    }
+
+
+}
