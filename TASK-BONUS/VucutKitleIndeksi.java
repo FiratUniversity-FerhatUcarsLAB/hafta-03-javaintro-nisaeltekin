b43a701a795e5594db0fa4b73b3793a04fb328a2
Nisanur <nisaeltekin@gmail.com>
2025-10-21 20:37:04 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,9 @@
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------");
+        System.out.println("Vücut Kitle İndeksi (BMI) Hesaplama");
+        System.out.println("----------------------------------");
+
+        System.out.println("Formül: BMI = kilo / (boy * boy)");
+    }
+}
