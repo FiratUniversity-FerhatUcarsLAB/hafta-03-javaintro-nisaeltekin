41ecacedac732b855a36533f89502d449b74c5fe
Nisanur <nisaeltekin@gmail.com>
2025-10-21 16:34:48 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,11 @@
+public class DaireHesap {
+    public  static void main(String[] args) {
+        double r = 5.5;
+        double pi = 3.1415;
+        double alan = pi * r * r;
+        double çevre = 2 * pi * r;
+        System.out.println("pi = " + pi);
+        System.out.println("çevre = " + çevre);
+        System.out.println("alan = " + alan);
+    }
+}
