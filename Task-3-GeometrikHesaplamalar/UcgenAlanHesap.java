6bb1bcefde7cf0ecda827c333acfd802f87818f3
Nisanur <nisaeltekin@gmail.com>
2025-10-21 16:36:07 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,11 @@
+public class UcgenAlanHesap {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = 5;
+
+        double s = (a + b + c) / 2.0;
+        double alan = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        System.out.println("alan = " + alan);
+    }
+}
