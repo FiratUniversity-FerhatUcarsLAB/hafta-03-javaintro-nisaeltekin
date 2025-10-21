009ec3203d4f43396cca6338febd853290060562
Nisanur <nisaeltekin@gmail.com>
2025-10-21 17:12:48 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,17 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+        String liste = """
+---------------------------------------
+| Ürün            | Miktar | Fiyat (₺) |
+---------------------------------------
+| Simit           |   2    |   15.00   |
+| Soda            |   1    |   22.50   |
+| Yumurta (10'lu) |   1    |   40.00   |
+| Erik            |   3    |   36.00   |
+---------------------------------------
+| Toplam                     113.50 ₺  |
+---------------------------------------
+""";
+        System.out.println(liste);
+    }
+}
