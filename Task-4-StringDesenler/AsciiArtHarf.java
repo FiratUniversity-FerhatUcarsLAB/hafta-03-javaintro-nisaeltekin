db630b8e323b350809ed0a666b4ac7f303899469
Nisanur <nisaeltekin@gmail.com>
2025-10-21 17:12:11 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,15 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        String ad = """
+*     *
+**    *
+* *   *
+*  *  *
+*   * *
+*    **
+*     *
+""";
+
+        System.out.println(ad);
+    }
+}
