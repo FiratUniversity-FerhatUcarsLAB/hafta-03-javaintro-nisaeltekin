3a603d5db4791802e1e662ee7561758aac8b74bd
Nisanur <nisaeltekin@gmail.com>
2025-10-21 17:11:35 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,14 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Nisanur";
+        String soyad = "Eltekin";
+        String ogrNo = "250541100";
+        String bolum = "Yazilim Muh.";
+
+        System.out.println("ad = " + ad);
+        System.out.println("soyad = " + soyad);
+        System.out.println("ogrNo = " + ogrNo);
+        System.out.println("bolum = " + bolum);
+
+    }
+}
