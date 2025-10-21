22fa90a2a3eefd4440e789d61fca152c0728d37d
Nisanur <nisaeltekin@gmail.com>
2025-10-21 20:14:25 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,13 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+        System.out.println("Dönüşüm Tablosu: Mil → Kilometre (1 mil = 1.609 km)");
+        System.out.println("-----------------------------------------");
+        System.out.println("Mil  Kilometre");
+
+        System.out.println("1     " + (1 * 1.609));
+        System.out.println("5     " + (5 * 1.609));
+        System.out.println("10    " + (10 * 1.609));
+        System.out.println("20    " + (20 * 1.609));
+        System.out.println("50    " + (50 * 1.609));
+    }
+}
